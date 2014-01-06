036121e4d6b02c613b35a02ef650a6be90a7cf22
Jesse Donat <j.donat@capstonedigital.com>
2014-01-06 16:24:20 -0600

    Start at object orienting the code

---
+++ src/donatj/MDDoc/MDDoc.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace donatj\MDDoc;
+
+use donatj\Flags;
+
+/**
+ * Application MDDoc
+ *
+ * @package donatj\MDDoc
+ */
+class MDDoc {
+
+	function __construct( $args ) {
+
+		$this->init($args);
+
+	}
+
+	private function init( $args ) {
+		$flags       = new Flags();
+		$displayHelp = & $flags->bool('help', false, 'Display this help message.');
+
+		try {
+			$flags->parse($args);
+		} catch(\Exception $e) {
+//			$ui->dropError($e->getMessage(), 1, $flags->getDefaults());
+			drop($e->getMessage(), 1, $flags->getDefaults());
+		}
+	}
+}
