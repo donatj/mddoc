eedf4ef1222bfcb2267b58e5f861037d27c07f0b
Jesse Donat <j.donat@capstonedigital.com>
2014-01-06 19:43:34 -0600

    Recursive Directory Tag roughed in

--- src/donatj/MDDoc/Documentation/RecursiveDirectory.php
+++ src/donatj/MDDoc/Documentation/RecursiveDirectory.php
@@ -12,11 +12,37 @@ namespace donatj\MDDoc\Documentation;
 class RecursiveDirectory extends AbstractNestedDoc {
 
 	function __construct( $name ) {
-		
+		foreach( $this->getFileList($name) as $file ) {
+			$this->addChild(new File((string)$file));
+		}
 	}
 
 	public function output( $depth ) {
-		// TODO: Implement output() method.
+		$output = '';
+		foreach($this->getChildren() as $child) {
+			$output .= $child->output($depth);
+		}
+
+		return $output;
+	}
+
+	private function getFileList( $path ) {
+		if( $real = realpath($path) ) {
+			$path = $real;
+		}
+
+		$path = rtrim($path, DIRECTORY_SEPARATOR);
+
+		if( is_dir($path) ) {
+			$dir   = new \RecursiveDirectoryIterator($path);
+			$ite   = new \RecursiveIteratorIterator($dir);
+			$files = new \RegexIterator($ite, "/\.php$/");
+
+			return $files;
+		} elseif( is_readable($path) ) {
+			return new \ArrayIterator(array( $path ));
+		}
+
 	}
 
 
